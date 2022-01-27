69c8afd3dbb39f06ded73b5b6ee1de88be23cdd2
Thenmozhi <82136801+ThenmozhiLawrence@users.noreply.github.com>
2022-01-27 00:17:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  "client", "build", "index.html"));
   });
 }
 
